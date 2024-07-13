67f8e27731e28908c1f3fa1b16bdeb267e3517b8
MatheeshaDias0926 <155762517+MatheeshaDias0926@users.noreply.github.com>
2024-07-13 15:17:47 +0530

    changes
    ...

---
+++ FirebaseConfig.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzBFHjghbuVamfM-zzUklzJageEN0-skU",
+  authDomain: "matheeesha-a958d.firebaseapp.com",
+  databaseURL: "https://matheeesha-a958d-default-rtdb.firebaseio.com",
+  projectId: "matheeesha-a958d",
+  storageBucket: "matheeesha-a958d.appspot.com",
+  messagingSenderId: "1065936756874",
+  appId: "1:1065936756874:web:5077f089ec2fe03010cd73"
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
